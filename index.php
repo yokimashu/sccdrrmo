f1d9e21fb2802e237c4388b62b6adc7191b395c6
Felix Michael Oberes <yokimashu@Felixs-MacBook-Pro.local>
2020-03-24 13:51:59 +0800

    ok

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href=""><b>LguSCC</b> Document Tracking System</a>
+    <a href=""><b>SCCDRRMO</b> Emergency Response Portal</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
