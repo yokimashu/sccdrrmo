0ada1c5abc6c62099dac6fff0aa74265bc03de2a
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2020-03-29 18:19:57 +0800

    index new update

--- index.php
+++ index.php
@@ -1,74 +1,76 @@
 <?php
-   include('config/db_config.php');
-//variable declaration
-   $alert_msg = '';
-   //sign in button
-    if (isset($_POST['signin'])){
-        //to check if data are passed
-        // echo "<pre>";
-        //     print_r($_POST);
-        // echo "</pre>";
-
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $check_username_sql = "SELECT * FROM tbl_users where username = :uname and status =:status";
+    include('config/db_config.php');
+    //variable declaration
+       $alert_msg = '';
+       //sign in button
+        if (isset($_POST['signin'])){
+            //to check if data are passed
+            // echo "<pre>";
+            //     print_r($_POST);
+            // echo "</pre>";
+    
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+    
+            $check_username_sql = "SELECT * FROM tbl_users where username = :uname and status =:status";
+            
+            $username_data = $con->prepare($check_username_sql);
+            $username_data ->execute([
+              ':uname' => $username,
+              ':status' => 'ACTIVE',
+            ]);
+              if ($username_data->rowCount() > 0){
+                while ($result = $username_data->fetch(PDO::FETCH_ASSOC)) {
+              
+                  //from database already hash
+                  $hash_password = $result['password'];
         
-        $username_data = $con->prepare($check_username_sql);
-        $username_data ->execute([
-          ':uname' => $username,
-          ':status' => 'ACTIVE',
-        ]);
-          if ($username_data->rowCount() > 0){
-            while ($result = $username_data->fetch(PDO::FETCH_ASSOC)) {
-          
-              //from database already hash
-              $hash_password = $result['password'];
+                  //hash the $u_pass and compared to $hashed_password
+                  if (password_verify($password, $hash_password)) {
+                   session_start();
+                   $_SESSION['id'] = $result['id'];
     
-              //hash the $u_pass and compared to $hashed_password
-              if (password_verify($password, $hash_password)) {
-               session_start();
-               $_SESSION['id'] = $result['id'];
-
-                if ($result['account_type'] == 1) {
-                  
-
-
-                  header('location: admin'); //location is folder
-                }
-                else {
-                  header('location: user');
-                }
-              }
-              else{
-                //echo "Password does not match!";
-                $alert_msg .= ' 
-                <div class="new-alert new-alert-warning alert-dismissible">
-                    <i class="icon fa fa-warning"></i>
-                    Incomplete Details!
-                </div>     
-            ';
-              }
+                    if ($result['account_type'] == 1) {
+                      
     
-                
+    
+                      header('location: admin'); //location is folder
+                    }
+                    else {
+                      header('location: user');
+                    }
+                  }
+                  else{
+                    //echo "Password does not match!";
+                    $alert_msg .= ' 
+                    <div class="new-alert new-alert-warning alert-dismissible">
+                        <i class="icon fa fa-warning"></i>
+                        Incomplete Details!
+                    </div>     
+                ';
+                  }
+        
+                    
+                }
+            }else{
+              $alert_msg .= ' 
+              <div class="new-alert new-alert-warning alert-dismissible">
+                  <i class="icon fa fa-warning"></i>
+                  Username does not exist!
+              </div>     
+          ';
             }
-        }else{
-          $alert_msg .= ' 
-          <div class="new-alert new-alert-warning alert-dismissible">
-              <i class="icon fa fa-warning"></i>
-              Username does not exist!
-          </div>     
-      ';
+    
+            
+          
+            
+    
         }
 
-        
-      
-        
-
-    }
   
 
 ?>
+
 
 
 
@@ -121,36 +123,80 @@
 
     <form role="form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
       <div class="form-group has-feedback">
-        <!-- alert here -->
-        <?php echo $alert_msg;?>
+        <!-- alert here
+                  <?php echo $alert_msg;?>
+                                <!-- ALERT BOX -->
+                                <?php
+                     if(isset($_SESSION['error'])){
+                     echo "
+                       <div class='alert alert-danger alert-dismissible'>
+                         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                           ".$_SESSION['error']."
+                       </div>
+                       ";
+                     unset($_SESSION['error']);
+                     }
+                     if(isset($_SESSION['check'])){
+                      echo "
+                        <div class='alert alert-danger alert-dismissible'>
+                          <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                            ".$_SESSION['check']."
+                        </div>
+                        ";
+                      unset($_SESSION['check']);
+                      }
+                     if(isset($_SESSION['success'])){
+                      echo "
+                      
+                       <div class='alert alert-success alert-dismissible'>
+                         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                           ".$_SESSION['success']."
+                       </div>
+                   
+                       ";
+                      unset($_SESSION['success']);
+                   }
+                 ?> 
       </div>
-      <div class="form-group has-feedback">
+
+      <div class="form-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text"><i class="fa fa-user"></i></span>
         <input type="text" class="form-control" name="username" placeholder="Username">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
-      <div class="form-group has-feedback">
+      </div>
+
+      <div class="form-group">
+      <div class="input-group-prepend">
+      <span class="input-group-text"><i class="fa fa-lock"></i></span>
         <input type="password" class="form-control" name="password" placeholder="Password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
+      </div>
+
+      <br>
 
       <div class="row">
         <div class="col-md-6">
-          <a href="register.php" class="btn btn-primary pull-left">Sign Up</a>
+          <a href="#addnew" data-toggle="modal" style ="color:white;"  data-backdrop="static"  class="btn btn-primary pull-left">Sign Up</a>
         </div>
         <div class="col-md-6">
           <input type="submit" class="btn btn-success pull-right" name="signin" value="Sign In">
         </div>
       </div>
+    
     </form>
-  </div>
-  <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
+  </div><!-- /.login-box-body -->
+ 
+</div><!-- /.login-box -->
+</body> 
 
+<?php include('adduser_modal.php'); ?>
 <!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
 
 </body>
+
 </html>
