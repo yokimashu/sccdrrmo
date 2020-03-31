9eff54de025d392693b97cd1d0d0d343a08380cf
em1730 <62753224+em1730@users.noreply.github.com>
2020-03-31 14:36:30 +0800

    Create view_pum.php

---
+++ admin/view_pum.php
@@ -0,0 +1,146 @@
+
+<?php
+
+include ('../config/db_config.php');
+include ('sql_pum.php');
+session_start();
+$user_id = $_SESSION['id'];
+
+if (!isset($_SESSION['id'])) {
+    header('location:../index.php');
+} else {
+
+}
+
+date_default_timezone_set('Asia/Manila');  
+$date = date('Y-m-d');
+$time = date('H:i:s');
+
+$symptoms= $patient='';
+
+//fetch user from database
+$get_user_sql = "SELECT * FROM tbl_users where id = :id";
+$user_data = $con->prepare($get_user_sql);
+$user_data->execute([':id' => $user_id]);
+while ($result = $user_data->fetch(PDO::FETCH_ASSOC)) {
+
+    $db_fullname = $result['fullname'];
+
+}
+
+
+
+
+?>
+
+ 
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SCCDRRMO | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../plugins/font-awesome/css/font-awesome.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="../plugins/iCheck/flat/blue.css">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="../plugins/datepicker/datepicker3.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap4.css">
+  <!-- Select2-->
+  <link rel="stylesheet" href="../plugins/select2/select2.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <?php include('sidebar.php');?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" >
+    <div class="content-header"></div>
+    
+    <section class="content">
+
+        
+    </section>
+    
+
+
+  </div>
+  <!-- /.content-wrapper -->
+ <?php include('footer.php')?>
+
+</div>
+
+<!-- add pum modal -->
+
+
+
+
+
+
+
+
+
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- datepicker -->
+<script src="../plugins/datepicker/bootstrap-datepicker.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<!-- Slimscroll -->
+<script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="../dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+<!-- DataTables -->
+<script src="../plugins/datatables/jquery.dataTables.js"></script>
+<!-- DataTables Bootstrap -->
+<script src="../plugins/datatables/dataTables.bootstrap4.js"></script>
+<!-- Select2 -->
+<script src="../plugins/select2/select2.full.min.js"></script>
+
+<script>
+     $('.select2').select2();
+
+    $('#users').DataTable({
+      'paging'      : true,
+      'lengthChange': true,
+      'searching'   : true,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : true,
+      'autoHeight'  : true
+     
+    });
+
+    $('#addPUM').on('hidden.bs.modal', function () {
+        $('#addPUM form')[0].reset();
+    });
+
+    $(function() {
+      $('[data-toggle="datepicker"]').datepicker({
+        autoHide: true,
+        zIndex: 2048,
+      });
+    });
+
+</script>
+</body>
+</html>
