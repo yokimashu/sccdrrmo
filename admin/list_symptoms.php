5ac008830b4c643e1df385c8001245510873c0b9
em1730 <62753224+em1730@users.noreply.github.com>
2020-03-31 17:46:37 +0800

    Update list_symptoms.php

--- admin/list_symptoms.php
+++ admin/list_symptoms.php
@@ -96,7 +96,7 @@ $get_all_symptoms_data->execute();
                                                             <a class="btn btn-success btn-xs" href=" ">
                                                             <i class="fa fa-folder-open-o"></i> Open
                                                             </a>
-                                                            <button class="btn btn-outline-danger btn-sm" data-role="confirm_delete" 
+                                                            <button class="btn btn-danger btn-sm" data-role="confirm_delete" 
                                                             data-id="<?php echo $list_symptoms["idno"]; ?>"><i class="fa fa-trash-o"></i></button>
                                                             &nbsp;                           
                                                             
