8b0701a31a958cecfcb7ab1cda08265e6383ee49
em1730 <62753224+em1730@users.noreply.github.com>
2020-03-31 16:31:35 +0800

    k

--- admin/sql_pum.php
+++ admin/sql_pum.php
@@ -44,6 +44,47 @@
 
     }
 
+    else if (isset($_POST['update_pum'])) {
+
+        $alert_msg = '';
+        $get_fullname = $_POST['fullname'];
+        $get_date = $_POST['report_date'];
+        $get_time = $_POST['report_time'];
+        $ge_symptomsss = $_POST['symptomlk'];
+        $get_id = $_POST['idno'];
+    
+       
+        $update_categ_sql = "UPDATE tbl_pum SET
+            date_report         = :datess,
+            fullname            = :namesss,
+            time_report         = :timesss,
+            symptoms            = :symp,
+            status              = :stat, 
+
+            where idno      = :id";
+                
+        $update_data = $con->prepare($update_categ_sql);
+        $update_data->execute([
+            ':datess'           => $get_id,
+            ':namesss'          => $get_category,
+            ':timesss'          => $get_code,
+            ':symp'             => $get_code,
+            ':stat'             => $get_status
+            ]);
+    
+            $alert_msg .= ' 
+            <div class="new-alert new-alert-success alert-dismissible">
+                <i class="icon fa fa-success"></i>
+                Data Updated.
+            </div>     
+          ';
+        header('location: view_pum.php');
+    
+    }
+
+
+
+
 
 
 
