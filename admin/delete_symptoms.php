0cbe3e8ca8b0defcb9f93f7ead384998c97b9248
em1730 <62753224+em1730@users.noreply.github.com>
2020-04-01 11:48:07 +0800

    Update delete_symptoms.php

--- admin/delete_symptoms.php
+++ admin/delete_symptoms.php
@@ -2,7 +2,7 @@
 if (isset($_POST['delete_symptoms'])) {
 
     $delete_user_id = $_POST['user_id'];
-    $delete_user_sql = "UPDATE tbl_pum SET status ='Inactive' WHERE idno = :id ";
+    $delete_user_sql = "UPDATE tbl_symptoms SET status ='Inactive' WHERE idno = :id ";
     $delete_user_data = $con->prepare($delete_user_sql);
     $delete_user_data->execute([':id'=>$delete_user_id]);
 
