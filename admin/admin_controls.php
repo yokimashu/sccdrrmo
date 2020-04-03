a14a55e9b0da6d50fce3c54b32f5df6e19d457cb
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2020-04-03 16:46:38 +0800

    creating admin controls and session.php

---
+++ admin/admin_controls.php
@@ -0,0 +1,13 @@
+<?php 
+
+$registration_list = '<a href="" class="nav-link ">
+<i class="nav-icon fa fa-exchange"></i>
+<p>
+  REGISTRATION LIST
+  <i class="right fa fa-angle-left"></i>
+</p>
+</a>';
+
+
+
+?>
