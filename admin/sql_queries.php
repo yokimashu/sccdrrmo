2ac6de57f13a2a1a7796182535cf1ca47d3c0bae
em1730 <62753224+em1730@users.noreply.github.com>
2020-04-09 18:12:43 +0800

    Update sql_queries.php

--- admin/sql_queries.php
+++ admin/sql_queries.php
@@ -80,8 +80,8 @@
         $alert_msg .= ' 
             <div class="alert alert-success alert-dismissible">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                <i class="fa fa-warning"></i>
-                <strong> Sucess! </strong> Data Inserted.
+                <i class="fa fa-check-double"></i>
+                <strong> Success ! </strong> Data Inserted.
             </div>      
         ';
         
