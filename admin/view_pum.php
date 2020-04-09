6394468db93e80fccd4d015483b9bb6904f82e7d
em1730 <62753224+em1730@users.noreply.github.com>
2020-04-09 21:11:18 +0800

    Update view_pum.php

--- admin/view_pum.php
+++ admin/view_pum.php
@@ -12,7 +12,8 @@ if (!isset($_SESSION['id'])) {
 
 $now = new DateTime();
 
-$btnSave = $btnEdit = $get_fName = $get_lName = $get_mName= $get_age = $get_gender='';
+$btnSave = $btnEdit = $get_fName = $get_lName = $get_mName= $get_age = $get_gender=
+$get_brgy='';
 $btnNew = 'hidden';
 
 
@@ -29,6 +30,7 @@ if (isset($_GET['objid'])) {
     $get_lName              = $result['last_name'];
     $get_age                = $result['age'];
     $get_gender             = $result['gender'];
+    $get_brgy               = $result['barangay'];
     
      
    
@@ -154,17 +156,17 @@ $get_all_health_data->execute();
                               <option >Select Gender</option>
                               <option <?php if ($get_gender == 'Female') echo 'selected'; ?> value="Female">Female </option>
                               <option <?php if ($get_gender == 'Male') echo 'selected'; ?> value="Male">Male </option>
-                            </select> 
-                            
+                            </select>   
                           </div>
-                           <!-- <div class="col-md-3 " >
-                            <select class="form-control select2" id="barangay" name="barangay" value="<?php echo $brgy;?>">
-                                <option selected="selected">Select Barangay</option>
-                                <?php while ($get_brgy = $get_all_brgy_data->fetch(PDO::FETCH_ASSOC)) { ?>
-                                <option value="<?php echo $get_brgy['barangay']; ?>"><?php echo $get_brgy['barangay']; ?></option>
-                                <?php } ?>
+                          <div class="col-md-3 " >
+                            <select class="form-control select2" readonly id="barangay" name="barangay" value="<?php echo $type; ?>">
+                              <option>Please select...</option>
+                                <?php while ($get_brgy_data = $get_all_brgy_data->fetch(PDO::FETCH_ASSOC)) { ?>
+                                <?php  $selected = ($get_brgy == $get_brgy_data['barangay'])? 'selected':''; ?>
+                                <option <?=$selected;?> value="<?php echo $get_brgy_data['barangay']; ?>"><?php echo $get_brgy_data['barangay']; ?></option> 
+                              <?php } ?>
                             </select>
-                          </div> -->
+                          </div>
                         </div><br> 
 
                         <!-- <div class="row" >
