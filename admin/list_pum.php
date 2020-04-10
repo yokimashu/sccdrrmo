771ad6d64d97f7f7507058f186c663f9eeb22e44
em1730 <62753224+em1730@users.noreply.github.com>
2020-04-10 21:24:41 +0800

    Update list_pum.php

--- admin/list_pum.php
+++ admin/list_pum.php
@@ -78,7 +78,7 @@ $get_all_symptoms_data->execute();
                                 
                                   <div class="table-responsive">
                                   <div class="row">
-                                    <div class="col-md-4" id="combo"></div>
+                                    <div class="col-md-3" id="combo"></div>
                                   </div>
                                   <br>
                                     
