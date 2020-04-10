b233ca19176dc9e12a7097c72eaaf341ec58223e
em1730 <62753224+em1730@users.noreply.github.com>
2020-04-10 11:35:08 +0800

    Update view_pum.php

--- admin/view_pum.php
+++ admin/view_pum.php
@@ -194,7 +194,7 @@ $get_all_health_data->execute();
                                 <div class="input-group-addon">
                                   <i class="fa fa-calendar"></i>
                                 </div>
-                                <input type="text" readonly class="form-control pull-right" id="datepicker" name="get_arrivals" placeholder="Date Arrival"  value="<?php echo $get_arrival;?>">
+                                <input type="text" readonly class="form-control pull-right" id="datepicker" name="get_arrivals" placeholder="Date Arrival"  value="<?php echo $get_arrival->format('m-d-Y');?>">
                             </div>
                           </div>
                         </div><br>
@@ -259,7 +259,7 @@ $get_all_health_data->execute();
                                   <i class="fa fa-calendar"></i>
                                 </div>
                                 <input type="text" readonly class="form-control pull-right" id="datepicker" name="get_process" placeholder="Date Process" 
-                                  value="<?php echo $get_date; ?>">
+                                  value="<?php echo $get_date->format('m-d-Y'); ?>">
                             </div>
                           </div>
                          
