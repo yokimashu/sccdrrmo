6e731311c4265d295a20fc03fd27e5d8ac038704
em1730 <62753224+em1730@users.noreply.github.com>
2020-04-10 16:21:39 +0800

    Update list_pum.php

--- admin/list_pum.php
+++ admin/list_pum.php
@@ -61,19 +61,24 @@ $get_all_symptoms_data->execute();
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper" >
-    <div class="content-header" style="float:right;" >
-    
-      <div class="col-md-3"> 
-                          <select class="form-control select2" id="heath_status"  name="health_status" value="<?php echo $health_status;?>">
-                                <!-- <option selected="selected">Health Status</option>
-                                <?php while ($get_health = $get_all_health_data->fetch(PDO::FETCH_ASSOC)) { ?>
-                                <option value="<?php echo $get_health['health_status']; ?>"><?php echo $get_health['health_status']; ?></option>
-                                <?php } ?> -->
-                            </select>
-                        </div>
-                   
+    <div class="content-header" style="float:right;" ></div>
+    <section class="content">
+      <div class="card">
+      
+        <div class="box-body">
+          <div class="col-md-3"> 
+            <select class="form-control select2" id="heath_status"  name="health_status" value="<?php echo $health_status;?>">
+              <option selected="selected">Health Status</option>
+              <?php while ($get_health = $get_all_health_data->fetch(PDO::FETCH_ASSOC)) { ?>
+              <option value="<?php echo $get_health['health_status']; ?>"><?php echo $get_health['health_status']; ?></option>
+              <?php } ?>
+            </select>
+          </div>
+        
+        </div>
       </div>
-    
+    </section>
+
     <section class="content">
             <div class="card card-info">
                     <div class="card-header  text-white bg-success">
