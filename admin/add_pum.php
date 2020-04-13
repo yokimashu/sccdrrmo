75835848264f3bf7910d23edd326ca3a5f23df84
em1730 <62753224+em1730@users.noreply.github.com>
2020-04-13 09:03:25 +0800

    Update add_pum.php

--- admin/add_pum.php
+++ admin/add_pum.php
@@ -7,6 +7,7 @@ include ('generate_pum.php');
 session_start();
 $user_id = $_SESSION['id'];
 
+include ('verify_admin.php');
 if (!isset($_SESSION['id'])) {
     header('location:../index.php');
 } else {}
