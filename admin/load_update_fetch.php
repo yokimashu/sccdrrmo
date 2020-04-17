5d1cdf662d4073bd53fd11006eb599cfe74ab580
Julius Mosquera <juliusmosquera14@gmail.com>
2020-04-17 18:46:09 +0800

    fetch

--- admin/load_update_fetch.php
+++ admin/load_update_fetch.php
@@ -17,18 +17,46 @@
   foreach($result2 as $row)
   {
      $output .= '
-     <div class="col-12 col-sm-6 col-md-12">
-     <div class="info-box btn-outline-danger mb-3">
-      <a href="view_incident.php?&id='.$row["objid"].'" class="Ashake info-box-icon bg-danger elevation-1"><span ><i class="fa fa-exclamation"></i></span></a>
-        <div class="info-box-content">
-          <span class="info-box-text"><h4>INCIDENT REPORT!</h4></span>
-          <span>DATE REPORTED:<b> '.$row["createdat"].' </b></span> <br>
-          <span>TYPE:<b> '.$row["type"].' </b>
-          SEVERITY:<b> '.$row["severity"].' </b></span> <br>
-          <span>ADDRESS:<b> '.$row["location_address"].' </b></span>
-        </div>
-     </div>
-     </div>
+     <div class="small-box modal-dialog">
+     <div class="modal-content bg-danger">
+           <form class="form-horizontal" id ="userform">  
+           <div class = "modal-body">
+             <div class = "container">
+               <div class="inner">
+           <h3>INCIDENT REPORT!</h3>
+           <div class="row">
+            <div class="col-2">
+             <div class="align-items-center">
+                 <a href="view_incident.php?&id='.$row["objid"].'" class="Ashake btn info-box-icon btn-dark elevation-1"><span ><i class="fa fa-exclamation"></i></span></a>
+             </div>
+            </div>
+            <div class="col-10">
+
+                 <div class="form-group">
+                   <p><b>TYPE:</b> '.$row["type"].' <b>SEVERITY:</b> '.$row["severity"].'</p>
+                 </div>
+
+                 <div class="form-group">
+                   <p><b>ADDRESS:</b> '.$row["location_address"].' </p>
+                 </div>
+
+                 <div class="form-group">
+                   <p><b>DATE REPORTED:</b> '.$row["createdat"].' </p>
+                 </div>
+             </div>
+          </div><!-- row -->
+               </div> <!-- inner -->
+               <div class="icon">
+                 <i class="fa fa-ambulance"></i>
+               </div>
+               </div> <!-- modal body -->
+             </div> <!-- container -->
+           </form> 
+         
+       </div> <!-- modal content -->
+     </div> <!-- modal dialog -->
+
+
    ';
 }
 
@@ -43,17 +71,40 @@
   foreach($result as $row)
   {
      $output .= '
-     <div class="col-12 col-sm-6 col-md-12">
-     <div class="info-box btn-outline-danger mb-3">
-      <a href="report_pum" class="Ashake info-box-icon bg-danger elevation-1"><span ><i class="fa fa-exclamation"></i></span></a>
-        <div class="info-box-content">
-          <span class="info-box-text">REPORT PUI/PUM</span>
-          <span>DATE REPORTED:<b> '.$row["createdat"].' </b></span> <br>
-          <span>ADDRESS:<b> '.$row["address"].' </b></span>
-        </div>
-     </div>
-     </div>
-   ';
+     <div class="small-box modal-dialog">
+     <div class="modal-content bg-danger">
+           <form class="form-horizontal" id ="userform">  
+           <div class = "modal-body">
+             <div class = "container">
+               <div class="inner">
+           <h3>REPORT PUI/PUM</h3>
+           <div class="row">
+            <div class="col-2">
+             <div class="align-items-center">
+                 <a href="report_pum" class="Ashake btn info-box-icon btn-dark elevation-1"><span ><i class="fa fa-exclamation"></i></span></a>
+             </div>
+            </div>
+            <div class="col-10">
+                 <div class="form-group">
+                   <p><b>ADDRESS:</b> '.$row["address"].' </p>
+                 </div>
+                 <div class="form-group">
+                   <p><b>DATE REPORTED:</b> '.$row["createdat"].' </p>
+                 </div>
+             </div>
+          </div><!-- row -->
+               </div> <!-- inner -->
+               <div class="icon">
+                 <i class="fa fa-ambulance"></i>
+               </div>
+               </div> <!-- modal body -->
+             </div> <!-- container -->
+           </form> 
+         
+       </div> <!-- modal content -->
+     </div> <!-- modal dialog -->
+'
+;
 }
 
 echo $output;
