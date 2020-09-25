15f96d44a537f4871976827dd1fe4ab3ecb99778
MONDEROFRITZ <fritzmondero1992@gmail.com>
2020-09-25 17:20:41 +0800

    e
    e

--- plugins/jasperreport/entity_id.php
+++ plugins/jasperreport/entity_id.php
@@ -19,7 +19,7 @@ $PHPJasperXML = new PHPJasperXML();
 // $PHPJasperXML->arrayParameter=array("employeeNo"=>'12345678');
 $xml = $PHPJasperXML->load_xml_file("entity_id.jrxml");
 // $PHPJasperXML->xml_dismantle($xml);
-$PHPJasperXML->sql = "SELECT * from tbl_individual  where entity_no =' . $entity . '";
+$PHPJasperXML->sql = "SELECT * from tbl_individual  where entity_no ='TYVOJR1413'";
 $PHPJasperXML->transferDBtoArray($server, $user, $pass, $db);
 $PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
 
