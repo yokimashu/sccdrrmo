c53e30cacc3552280ef5a0927cde5cba28c241fe
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2020-10-06 16:06:11 +0800

    Update add_land_trans.php

--- admin/add_land_trans.php
+++ admin/add_land_trans.php
@@ -220,31 +220,63 @@ $title = 'VAMOS | Land Trans Form';
 
                                         <div class="box-body">
                                             <br>
-                                            <div class="row">
-                                                <div class="col-md-1"></div>
-                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                <div class="col-md-3">
+                                               <div class="row col-12">
 
-                                                    <div stytle="display: table-cell; vertical-align: middle; height: 50px; border: 1px solid red;" id="my_camera" align="center" onClick="setup()"> Click to ACCESS Camera</div><br>
 
+
+
+                                                <!-- <div class="col-12" style="vertical-align: middle; height: 280px; width:300px;border: 1px solid black ;" id="my_camera" align="center" onClick="setup()">
+                                                <img src="" id = "photo" style="margin:auto;height: 200px; width:280;"onClick="setup()">
+                                                        Click me to Open Camera
+                                                  
+                                                  
+                                                </div> -->
+                                                <div style="margin:auto">
+
+                                                    <video id="webcam" autoplay playsinline width="600" height="530" align="center" hidden class="photo  img-thumbnail"></video>
+                                                    <canvas id="canvas" class="d-none" hidden width="600" height="530" align="center" onClick="setup()" class="photo  img-thumbnail"></canvas>
+                                                    <audio id="snapSound"  src="audio/snap.wav"  preload="auto"></audio>
+
+                                                    <img src="../flutter/images/user.jpg" id="photo" style="height: 300px; width:500px;margin:auto;" class="photo img-thumbnail">
+                                                </div>
+
+                                            </div>
+                                               
+                                          <div class="row">
+                                              
+                                            <!-- <form method="POST" action="storeImage.php"> -->
+                                            <div style="margin:auto">
+                                                <div class="col-12" style="margin:auto;margin-top:30px;margin-bottom:30px">
+                                                    <span class="align-baseline">
+                                                        <input type="hidden" name="image" class="image-tag">
+                                                        <!-- <input type="button" class="btn btn-primary" value="&#9654" onClick="setup()">  -->
+                                                        <button type="button" <?php echo $btn_enabled ?> id="opencamera" class="btn btn-warning " value="CAPTURE"><i class="fa fa-camera"></i></button>
+                                                        <button type="button" <?php echo $btn_enabled ?> id="capture" class="btn btn-primary toastsDefaultSuccess" value="CAPTURE" onClick="take_snapshot()"><i class="fa fa-check"></i></button>
+<!--                                                        <button type="button" <?php echo $btn_enabled ?> id="crop" class="btn btn-primary toastsDefaultSuccess" value="CAPTURE" onClick="">CROP</button>-->
+                                                        <style>
+                                                            input[type="file"] {
+                                                                display: none;
+                                                            }
+
+                                                            .custom-file-upload {
+                                                                border: 1px solid #ccc;
+                                                                border-radius: 5px;
+                                                                display: inline-block;
+                                                                padding: 7px 12px;
+                                                                cursor: pointer;
+                                                            }
+                                                        </style>
+                                                        <label for="fileToUpload" class="custom-file-upload">
+                                                            <i class="fa fa-cloud-upload"></i> Import Image
+                                                        </label>
+                                                        <input type="file" <?php echo $btn_enabled ?> id="fileToUpload" name="myFile" class="btn btn-danger custom-file-upload ">
+
+                                                    </span>
                                                 </div>
                                             </div>
-
-                                            <div class="row" align="center">
-                                                <form method="POST" action="storeImage.php">
-
-                                                    <div class="col-md-3"></div>
-                                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                    <div>
-
-                                                        <!-- <input type="button" class="btn btn-primary" value="&#9654" onClick="setup()">  -->
-                                                        <input type="button" class="btn btn-primary" value="CAPTURE" onClick="take_snapshot()">
-                                                        <input type="button" class="btn btn-danger" value="IMPORT" onClick="take_snapshot()">
-
-                                                    </div>
-                                                </form>
-                                            </div><br>
+                                            <!-- </form> -->
+                                        </div>
+                                            
 
                                             <div class="row">
                                                 <div class="col-md-1"></div>
@@ -338,9 +370,9 @@ $title = 'VAMOS | Land Trans Form';
     <!-- Toastr -->
     <script src="../plugins/toastr/toastr.min.js"></script>
     <!-- Select2 -->
-
+ <script src="../plugins/cameracapture/webcam-easy.min.js"></script>
     <!-- <script src="../plugins/webcamjs/webcam.js"></script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
+<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>-->
     <!-- textarea wysihtml style -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <!-- <script src="jpeg_camera/jpeg_camera_with_dependencies.min.js" type="text/javascript"></script> -->
