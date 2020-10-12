423eba17dea4e7ba33f4acbe2eeb9dccedd73683
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2020-10-12 11:48:22 +0800

    push sidebar and user_controls;

--- admin/user_controls.php
+++ admin/user_controls.php
@@ -1,14 +1,22 @@
 <?php 
-
-$registration_list =  '<a href="list_users" class="nav-link">
+ $incident_report  = '';
+ $registration_list ='';
+ //sidebar buttons
+if ($_SESSION['user_type'] == 1) {
+    $registration_list =  '<a href="list_users" class="nav-link">
                         <i class="fa fa-minus nav-icon"></i>
                         <p>Users</p>
                         </a>';
 
-$incident_report ='<a href="list_incident" class="nav-link ">
+    $incident_report ='<a href="list_incident" class="nav-link ">
             <i class="nav-icon fa fa-book"></i>
             <p>
         INCIDENT REPORT 
         </p>
         </a>';
+
+// individual form 
+$view_history = '<a class="btn btn-success btn-sm view_history" id = "view_history" >
+<i class="fa fa-suitcase"></i></a>';
+}
 ?>
