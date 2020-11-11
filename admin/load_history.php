ea483d8afcd20eb8d101a160fa6c2a5ac90948b2
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2020-11-11 16:07:52 +0800

    added date range for the view history

--- admin/load_history.php
+++ admin/load_history.php
@@ -1,31 +1,12 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-include_once("PHPJasperXML.inc.php");
+include('../config/db_config.php');
+if (isset($_POST['entity_no'])) {
+    $entity_no =   $_POST['entity_no'];
+    $date_from =  date('Y-m-d', strtotime($_POST['date_from']));
+    $date_to =    date('Y-m-d', strtotime($_POST['date_to']));
 
 
-
-// $server = 'http://localhost:8888';
-// $user = 'root';
-// $pass = '1234';
-// $db = 'sccdrrmo';
-$host = "127.0.0.1";
-$db_name = "sccdrrmo";
-$username = "root";
-$password = "0Fd8xWc1anuE";
-$entity_no = $_GET['entity_no'];
-$datefrom =   date('Y-m-d', strtotime($_POST['date_from']));
-$dateto =  date('Y-m-d', strtotime($_POST['date_to']));
-$PHPJasperXML = new PHPJasperXML();
-// $PHPJasperXML->debugsql=true;
-// $PHPJasperXML->arrayParameter=array("employeeNo"=>'12345678');
-$xml = $PHPJasperXML->load_xml_file("individual_history.jrxml");
-// $PHPJasperXML->xml_dismantle($xml);
-// $PHPJasperXML->sql = "SELECT r.trace_no,r.date,t.fullname from tbl_tracehistory r inner join tbl_individual t on t.entity_no = r.trace_no where  r.entity_no ='" . $entity_no . "'";
-
-$PHPJasperXML->sql = "Select * FROM
+    $get_all_history_sql = "Select * FROM
 (
     SELECT date, time, t.entity_no, t.trace_no, i.fullname, CONCAT(i.street, ', ', i.barangay) as details, i.mobile_no FROM `tbl_tracehistory` t
 inner join tbl_individual i on i.entity_no = t.entity_no  WHERE t.entity_no = '" . $entity_no . "' AND t.date between '".$date_from."' and '".$date_to."' or t.trace_no = '" . $entity_no . "' AND t.date between '".$date_from."' and '".$date_to."'
@@ -70,9 +51,35 @@ WHERE fullname NOT IN (Select fullname from tbl_individual where entity_no = '" 
 )
 ORDER BY date DESC, time DESC";
 
-$PHPJasperXML->transferDBtoArray($host, $username, $password, $db_name);
-$PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
+    $get_all_history_data = $con->prepare($get_all_history_sql);
+    $get_all_history_data->execute();
 
+    
+    while ($list_history = $get_all_history_data->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr>";
+        echo "<td>";
+        echo $list_history['entity_no'];
+        echo "</td>";
+        echo "<td>";
+        echo $list_history['date'];
+        echo "</td>";
+        echo "<td>";
+        echo $list_history['time'];
+        echo "</td>";
+        echo "<td>";
+        echo $list_history['fullname'];
+        echo "</td>";
+        echo "<td>";
+        echo $list_history['details'];
+        echo "</td>";
+        echo "<td>";
+        echo $list_history['mobile_no'];
+        echo "</td>";
+        echo "</tr>";
+
+    }
+ 
+}
 
 
 ?>
