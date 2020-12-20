c922244a04e9ed59221c753ee6fdd7b903f0a960
em1730 <62753224+em1730@users.noreply.github.com>
2020-12-20 17:51:04 +0800

    Update footer.php

--- admin/footer.php
+++ admin/footer.php
@@ -8,7 +8,7 @@ $curYear = date('Y');
 ?>
 
 <footer class="main-footer">
-    <strong>Copyright &copy; <?php echo $curYear; ?> ITCSO <a href="http://lguscc.gov.ph">Local Government of San Carlos City</a>.</strong>
+    <strong>Copyright &copy; 2020 ITCSO <a href="http://lguscc.gov.ph">Local Government of San Carlos City</a>.</strong>
    
     <div class="float-right d-none d-sm-inline-block">
         All rights reserved.
