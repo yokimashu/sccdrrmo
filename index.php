58d2667f17e9d6fce9c1948c950908656de810a9
yokimashu <62580415+yokimashu@users.noreply.github.com>
2021-03-04 17:03:31 +0800

    ok

--- index.php
+++ index.php
@@ -259,6 +259,16 @@ if (isset($_POST['signin'])) {
 
   });
 </script>
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.2.9/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+
 </body>
 
 </html>
