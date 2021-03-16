8239ba17bb5ab2539dadeec50face936f32b0ce1
Jerhod Kyan A. Ricabo <jerhodkyan@gmail.com>
2021-03-16 10:52:58 +0800

    vaccinator and bakuna center sidebar
    s

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -291,6 +291,8 @@ $numberofnewreport = $get_all_newreport_data->rowCount();
         <?php echo $list_vaccine; ?>
         <?php echo $list_assessment; ?>
         <?php echo $list_categorylist; ?>
+        <?php echo $list_bakuna_center; ?>
+        <?php echo $list_vaccinator; ?>
         <?php echo $single_break; ?>
         <!-- end of masterlist of covid-19 -->
 
