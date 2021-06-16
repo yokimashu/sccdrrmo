98db468a3a6d46554ef22ff5caa07a042231099b
ralphjuvy <scchitprog@gmail.com>
2021-06-15 18:31:15 -0800

    chat config - group / private
    config for chat

---
+++ config/db_chatconfig.php
@@ -0,0 +1,35 @@
+<?php
+	
+	if (session_status() == PHP_SESSION_NONE) {
+		session_start();
+	}
+	
+	
+	//local
+	//$dbHost = "localhost";
+	//$dbName = "sccdrrmo";
+	//$dbUser = "root";
+	//$dbPasw = "";
+	
+	
+	//server local port
+	// $host = "127.0.0.1:3307";
+	// $db_name = "sccdrrmo";
+	// $username = "root";
+	// $password = "5zAhx4Et37Wr";
+	
+	//server
+	$dbHost = "127.0.0.1";
+	$dbName = "sccdrrmo";
+	$dbUser = "root";
+	$dbPasw = "I0nvNUWNXoYI";
+	
+	try {
+		$connect = new PDO("mysql:host={$dbHost};dbname={$dbName}", $dbUser, $dbPasw);
+		$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
+	
+	
+?>
