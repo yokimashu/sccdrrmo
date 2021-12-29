ac8817f01c983268a3c9a3ade74e618a1f22e221
itsemelona <62753224+em1730@users.noreply.github.com>
2021-12-29 12:01:35 +0700

    a

--- config/db_config.php
+++ config/db_config.php
@@ -1,10 +1,10 @@
 <?php
 
 //local
-$host = "localhost";
-$db_name = "sccdrrmo";
-$username = "root";
-$password = "";
+//$host = "localhost";
+//$db_name = "sccdrrmo";
+//$username = "root";
+//$password = "1234";
 
 //server local port
 // $host = "127.0.0.1:3307";
@@ -13,10 +13,10 @@ $password = "";
 // $password = "5zAhx4Et37Wr";
 
 //server
-// $host = "127.0.0.1";
-// $db_name = "sccdrrmo";
-// $username = "root";
-// $password = "I0nvNUWNXoYI";
+$host = "127.0.0.1";
+$db_name = "sccdrrmo";
+$username = "root";
+$password = "I0nvNUWNXoYI";
 
 try {
     //database connection
